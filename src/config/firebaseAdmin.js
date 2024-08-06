442795ce8f41c61361d077aa869b8b68700a4516
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-08-06 20:58:02 +0530

    fix secrets firebase

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,6 +1,5 @@
 const admin = require('firebase-admin');
 const path = require('path');
-const fs = require('fs');
 require('dotenv').config(); // Load environment variables
 
 
@@ -10,12 +9,7 @@ if (!serviceAccountBase64) {
   throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_KEY_BASE64 environment variable');
 }
 
-// Decode the base64 string and write it to a temporary file
-const serviceAccountBuffer = Buffer.from(serviceAccountBase64, 'base64');
-const serviceAccountFilePath = path.join(__dirname, 'serviceAccountKey.json');
-fs.writeFileSync(serviceAccountFilePath, serviceAccountBuffer);
-
-const serviceAccount = require(serviceAccountFilePath);
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
 
 
 // Initialize Firebase Admin with your service account
